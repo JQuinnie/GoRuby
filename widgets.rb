eb795457c6d7c36c0a0ffb99ca24bea09241b8b9
JQuinnie <jenn8866@gmail.com>
2018-07-27 11:40:51 -0600

    Create a method and return user input

--- widgets.rb
+++ widgets.rb
@@ -2,7 +2,14 @@
 puts "Welcome to the Widget Store!"
 
 # Ask for quantity
+# puts will skip down to next line, so use print to retrieve input
+def ask(question)
+  print question
+  answer = gets
+  puts answer
+end
 
+ask("How many widgets are you ordering? ")
 
 # Calculate total
 
