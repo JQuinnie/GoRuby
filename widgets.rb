54e56e6e50e37002383d3ac03faf15ea054e527b
JQuinnie <jenn8866@gmail.com>
2018-07-27 11:46:24 -0600

    Save method user response to a variable

--- widgets.rb
+++ widgets.rb
@@ -3,13 +3,14 @@ puts "Welcome to the Widget Store!"
 
 # Ask for quantity
 # puts will skip down to next line, so use print to retrieve input
+# Ruby does not need a return value, the last expression is the return value of the method
 def ask(question)
   print question
-  answer = gets
-  puts answer
+  gets
 end
 
-ask("How many widgets are you ordering? ")
+answer = ask("How many widgets are you ordering? ")
+puts answer
 
 # Calculate total
 
