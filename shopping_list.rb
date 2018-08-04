e931ae8dfcb48ea6f83e6b07cd8f21edf448fe3a
JQuinnie <jenn8866@gmail.com>
2018-08-04 17:32:20 -0600

    Create a grocery list

---
+++ shopping_list.rb
@@ -0,0 +1,21 @@
+# create a list
+def create_list
+  print "What is the list name? "
+  name = gets.chomp
+
+  hash = { "name" => name, "items" => Array.new }
+  return hash
+end
+
+# add item to list
+def add_list_item
+  print "What is the item called? "
+  item_name = gets.chomp
+
+  hash = {"name" => name }
+  return hash
+end
+
+list = create_list()
+puts list.inspect
+
