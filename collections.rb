45846b4ea65d5fd61e2fa53f67bec1d2f5f4dc89
JQuinn MacBook Air <jenn8866@gmail.com>
2018-08-03 21:45:12 -0600

    Explore collections by creating an array

---
+++ collections.rb
@@ -0,0 +1,10 @@
+# array
+grocery_list = ["milk", "egg", "bread"]
+# array of strings
+farmers_list = %w(apples oranges tomatoes)
+# array of strings with interpolation
+item = "honey"
+farmers_list = %W(#{item} apples oranges tomatoes)
+
+p grocery_list
+p farmers_list
