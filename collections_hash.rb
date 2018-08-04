35c8b74eb44eb800d378d32a5861770fa3851021
JQuinnie <jenn8866@gmail.com>
2018-08-04 16:58:11 -0600

    Add methods to get values

--- collections_hash.rb
+++ collections_hash.rb
@@ -5,8 +5,9 @@ item["brand"] = "Off Brand"
 item.store("calories", 120)
 p item
 
-# see all keys in hash
+# see all keys and values in hash
 p item.keys
+p item.values
 
 # does the hash have a certain key?
 p item.has_key?("brand")
@@ -16,3 +17,8 @@ p item.member?("brand")
 # get value with key
 p item.fetch("brand")
 p item["brand"]
+
+# does the hash have a certain value?
+p item.has_value?(1)
+p item.value?(1)
+p item.values_at("name", "quantity")
