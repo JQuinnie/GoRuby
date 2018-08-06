2a21e8442aa42e8f052a03b894c59dd90be5819c
JQuinnie <jenn8866@gmail.com>
2018-08-06 14:27:03 -0600

    Add random guess with loop conditionals

--- iterations.rb
+++ iterations.rb
@@ -1,8 +1,24 @@
 # create a loop
+# loop do
+#   print 'Do you want to continue? (y/n) '
+#   answer = gets.chomp.downcase
+#   if answer == 'n'
+#     break
+#   end
+# end
+
+# loop conditional
+random_number = Random.new.rand(5)
 loop do
-  print 'Do you want to continue? (y/n) '
-  answer = gets.chomp.downcase
-  if answer == 'n'
+  print 'Guess the number between 0 and 5 (e to exit): '
+  answer = gets.chomp
+  if answer == 'e'
+    puts "The number was #{random_number}."
     break
+  elsif answer.to_i == random_number
+    puts 'You guessed correctly!'
+    break
+  else
+    puts 'Try again.'
   end
 end
