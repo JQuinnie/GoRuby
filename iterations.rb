f8931136c4016454fb83ad0a7fdca47e25489dcd
JQuinnie <jenn8866@gmail.com>
2018-08-07 12:04:04 -0600

    Add with while loops

--- iterations.rb
+++ iterations.rb
@@ -30,22 +30,44 @@
 #   break if answer > 10
 # end
 
-# Program to loop through asking for someone's name and make sure formatting is correct
-def get_name
-  name = ''
-  loop do
-    print 'Enter your name (min. 2 characters, no numbers): '
-    name = gets.chomp
-    # check for name to be at least two characters and does not contain any numbers (digits)
-    if name.length >= 2 && !name.index(/\d/)
-      break
-    #if criteria is not met, then print out below
-    else
-      puts 'Name must be longer than 2 characters and not contain numbers.'
-    end
-  end
-  return name
+# # Program to loop through asking for someone's name and make sure formatting is correct
+# def get_name
+#   name = ''
+#   loop do
+#     print 'Enter your name (min. 2 characters, no numbers): '
+#     name = gets.chomp
+#     # check for name to be at least two characters and does not contain any numbers (digits)
+#     if name.length >= 2 && !name.index(/\d/)
+#       break
+#     #if criteria is not met, then print out below
+#     else
+#       puts 'Name must be longer than 2 characters and not contain numbers.'
+#     end
+#   end
+#   return name
+# end
+
+# name = get_name()
+# puts "Hi #{name}."
+
+# while loop, does not need the break key word
+answer =''
+while answer !='n'
+  print 'Do you want me to repeat this pointless loop again? (y/n) '
+  answer = gets.chomp.downcase
 end
 
-name = get_name()
-puts "Hi #{name}."
+def print_hello(number_of_times)
+  i = 0
+  while i < number_of_times
+    puts 'hello'
+    i += 1
+  end
+end
+
+answer_more = 0
+while answer_more < 5
+  print "How many times do you want to print 'hello'? (Enter a number greater than 5 to exit) "
+  answer_more = gets.chomp.to_i
+  print_hello(answer_more)
+end
