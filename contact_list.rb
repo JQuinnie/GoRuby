333ac64bf644caaa13b79683f8d06e2f98fd181d
JQuinnie <jenn8866@gmail.com>
2018-08-08 15:15:06 -0600

    Create simple app for contact list

---
+++ contact_list.rb
@@ -0,0 +1,12 @@
+# a simple contact list program
+contact_list= []
+
+def ask(question, kind='string')
+  print question + " "
+  answer = gets.chomp
+  answer = answer.to_i if kind == 'number'
+  return answer
+end
+
+answer = ask('What is your name?')
+puts answer
