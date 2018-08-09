81f79c3d9601468ebbe9b60b72549da24b5fc82a
JQuinnie <jenn8866@gmail.com>
2018-08-09 10:27:49 -0600

    Create classes and objects examples

---
+++ classes.rb
@@ -0,0 +1,24 @@
+# creation of a class, it is composed of methods and variables.
+# Take a 'Name' class to take a variable and share it
+class Name
+  def initialize(title, first_name, middle_name, last_name)
+    @title = title
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
+  end
+
+  attr_reader :title
+
+  attr_reader :first_name
+
+  attr_reader :middle_name
+
+  attr_reader :last_name
+end
+
+name = Name.new('Ms.', 'Jenn', 'N', 'Chu')
+puts name.title + ' ' +
+     name.first_name + ' ' +
+     name.middle_name + ' ' +
+     name.last_name
