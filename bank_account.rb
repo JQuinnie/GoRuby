f9d97d8035ff94524141303c3deb5603a7f74afc
JQuinnie <jenn8866@gmail.com>
2018-08-10 10:21:59 -0600

    Create method to add transactions to bank account

--- bank_account.rb
+++ bank_account.rb
@@ -1,9 +1,17 @@
 class BankAccount
+  attr_reader :name
   # initialize method takes one argument and sets @name as an instance variable on initialization
   def initialize(name)
-    @name = Name
+    @name = name
     @transactions = []
+  end
+
+  # method for adding transaction
+  def add_transaction(description, amount)
+    @transactions.push(description: description, amount: amount)
   end
 end
 
 bank_account = BankAccount.new('client')
+bank_account.add_transaction('Groceries', 40)
+puts bank_account.inspect
