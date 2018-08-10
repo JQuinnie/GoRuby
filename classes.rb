026089b51a0be3020ec947c42f02a70957f7d388
JQuinn MacBook Air <jenn8866@gmail.com>
2018-08-09 23:38:01 -0600

    Update using method and simplifying

--- classes.rb
+++ classes.rb
@@ -8,19 +8,23 @@ class Name
     @last_name = last_name
   end
 
-  # def title
-    # @title
-  # end
-  attr_reader :first_name, :middle_name, :last_name
+  # create method
+  def full_name
+    @first_name + " " + @middle_name + " " + @last_name
+  end
+
+  def full_name_with_title
+    @title + ' ' + full_name()
+  end
+
   # allows attr_reader and attr_writer
-  attr_accessor :title
+  attr_accessor :title, :first_name, :middle_name, :last_name
 end
 
 name = Name.new('Ms.', 'Jenn', 'N', 'Chu')
-puts name.title + ' ' +
-     name.first_name + ' ' +
-     name.middle_name + ' ' +
-     name.last_name
+puts name.full_name_with_title
+name = Name.new('Mr.', 'Johnny', 'H', 'Chu')
+puts name.full_name_with_title
 
 puts "Previous title: #{name.title}"
 name.title = 'Dr.'
