f6cb9b774698f8061bf0b41010864017659349a8
JQuinnie <jenn8866@gmail.com>
2018-08-15 10:30:51 -0600

    Update quiz to work

--- fruity_quiz.rb
+++ fruity_quiz.rb
@@ -6,12 +6,27 @@ class Question
   end
 end
 
-q1 = "Bananas grow on trees.\n(a)True\n(b)False"
-q2 = "Granny Smith apples are what color?\n(a)red\n(b)green\n(c)orange"
-q3 = "What is the best season to pick strawberries?\n(a)February\n(b)July\n(c)October"
+q1 = "1) Bananas grow on trees.\n(a)True\n(b)False"
+q2 = "2) Granny Smith apples are what color?\n(a)red\n(b)green\n(c)orange"
+q3 = "3) What is the best season to pick strawberries?\n(a)February\n(b)July\n(c)October"
 
 questions = [
   Question.new(q1, "a"),
   Question.new(q2, "b"),
   Question.new(q3, "b")
 ]
+
+def run_test(questions)
+  answer = ''
+  score = 0
+  for question in questions
+    puts question.prompt
+    answer = gets.chomp()
+    if answer == question.answer
+      score += 1
+    end
+  end
+  puts 'You got ' + score.to_s + '/' + questions.length().to_s + ' correct'
+end
+
+run_test(questions)
