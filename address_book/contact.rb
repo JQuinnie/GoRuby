ed748d218757be5b8330292d2592e755bd44cbf2
JQuinnie <jenn8866@gmail.com>
2018-08-17 12:19:13 -0500

    Add additional methods and include a to string method as well

--- address_book/contact.rb
+++ address_book/contact.rb
@@ -13,6 +13,22 @@ class Contact
     @last_name
   end
 
+  def first_last
+    first_name + ' ' + last_name
+  end
+
+  def last_first
+    last_first = last_name
+    last_first += ', '
+    last_first += first_name
+    unless @middle_name.nil?
+      last_first += ' '
+      last_first += middle_name.slice(0, 1)
+      last_first += '.'
+    end
+    last_first
+  end
+
   def full_name
     full_name = first_name
     unless @middle_name.nil?
@@ -22,6 +38,21 @@ class Contact
     full_name += ' '
     full_name += last_name
     full_name
+  end
+
+  def to_s(format = 'full_name')
+    case format
+    when 'full_name'
+      full_name
+    when 'last_first'
+      last_first
+    when 'first'
+      first_name
+    when 'last'
+      last_name
+    else
+      first_last
+    end
   end
 end
 
@@ -33,5 +64,6 @@ contact_two = Contact.new
 contact_two.first_name = 'Johnny'
 contact_two.last_name = 'Chu'
 
-puts contact_one.full_name
-puts contact_two.full_name
+puts contact_one.to_s
+puts contact_two.to_s('full_name')
+puts contact_one.to_s('last_first')
