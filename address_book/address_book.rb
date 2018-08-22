8063f6cd5db70fabdb7ebbc58911c3fa7a410c28
JQuinnie <jenn8866@gmail.com>
2018-08-22 17:55:43 -0600

    Refactor by creating one method to print all results

--- address_book/address_book.rb
+++ address_book/address_book.rb
@@ -9,6 +9,17 @@ class AddressBook
     @contacts = []
   end
 
+  # refactor print results
+  def print_results(search, results)
+    puts search
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      contact.print_phone_numbers
+      contact.print_addresses
+      puts "\n"
+    end
+  end
+
   # search method for name
   def find_by_name(name)
     results = []
@@ -18,13 +29,7 @@ class AddressBook
         results.push(contact)
       end
     end
-    puts "Name search results (#{search})"
-    results.each do |contact|
-      puts contact.to_s('full_name')
-      contact.print_phone_numbers
-      contact.print_addresses
-      puts "\n"
-    end
+    print_results("Name search results (#{search})", results)
   end
 
   # search method for phone number
@@ -36,17 +41,11 @@ class AddressBook
       # inner loop to loop thru contacts phone numbers
       contact.phone_numbers.each do |phone_number|
         if phone_number.number.gsub('-', '').include?(search)
-          results.push(contact)
+          results.push(contact) unless results.include?(contact)
         end
       end
     end
-    puts "Phone search results (#{search})"
-    results.each do |contact|
-      puts contact.to_s('full_name')
-      contact.print_phone_numbers
-      contact.print_addresses
-      puts "\n"
-    end
+    print_results("Phone search results (#{search})", results)
   end
 
   def print_contact_list
