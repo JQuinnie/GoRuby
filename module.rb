9516b75d92cf320661c5a1eedbbfb9692e565324
JQuinnie <jenn8866@gmail.com>
2018-08-25 21:31:38 -0600

    Add new module that contains multi levels

--- module.rb
+++ module.rb
@@ -6,3 +6,23 @@
 module SimpleModule
   VERSION = 1.0
 end
+
+# multiple levels of modules can be used to namespace items:
+module LaserBots
+  module Console
+    class Command
+    end
+  end
+
+  module World
+    class Player
+      attr_reader :name
+      def initialize(name)
+        @name = name
+      end
+    end
+  end
+end
+# classes, modules, and contants can then be accessed by using the constant resolution operator(::) to get to nested namespace:begin
+player = LaserBots::World::Player.new("Player 1")
+puts player.name
