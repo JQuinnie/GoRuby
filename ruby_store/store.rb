dc0b0011e4fd5e632bca8596faed0da76f1fd877
JQuinnie <jenn8866@gmail.com>
2018-09-02 10:46:50 -0600

    class inside module to extend itself of all methods inside the module

--- ruby_store/store.rb
+++ ruby_store/store.rb
@@ -1,12 +1,16 @@
 # A simple program that simulates keeping inventory at a small retail store.
 # Module to track inventory and hold inventory methods
 module Inventoryable
+  # tell the class to extend itself with the class methods that are inside the module
+  def self.included(klass)
+    klass.extend(ClassMethods)
+  end
 
   module ClassMethods
     def create(attributes)
       object = new(attributes)
       instances.push(object)
-      return object
+      object
     end
 
     def instances
@@ -18,10 +22,12 @@ module Inventoryable
   def stock_count
     @stock_count ||= 0
   end
+
   # set stock count
   def stock_count=(number)
     @stock_count = number
   end
+
   # see if item is in stock
   def in_stock?
     stock_count > 0
@@ -29,7 +35,6 @@ module Inventoryable
 end
 
 class Shirt
-  extend Inventoryable::ClassMethods
   include Inventoryable
   attr_accessor :attributes
 
@@ -54,8 +59,8 @@ class Accessory
   end
 end
 
-shirt1 = Shirt.create(name: "MTF", size: "L")
-shirt2 = Shirt.create(name: "MTF", size: "M")
+shirt1 = Shirt.create(name: 'MTF', size: 'L')
+shirt2 = Shirt.create(name: 'MTF', size: 'M')
 
 shirt1.stock_count = 10
 
