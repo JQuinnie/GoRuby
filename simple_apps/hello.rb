d3d8691dcd6d4e0cc87b4afc5d4d6d1827e9da95
jquinnie <jenn8866@gmail.com>
2020-07-10 21:52:43 -0600

    add a function to hello

--- simple_apps/hello.rb
+++ simple_apps/hello.rb
@@ -1,1 +1,11 @@
-puts 'Hello, Ruby!'
+puts 'Hello, Ruby programmer!'
+puts 'Can you tell me your name?'
+
+dev_name = gets.chomp
+
+def say_hello(name)
+  puts "Hi #{name}! Hope you have fun programming in Ruby!"
+  puts "Your lucky- number is #{name.length}."
+end
+
+say_hello dev_name
